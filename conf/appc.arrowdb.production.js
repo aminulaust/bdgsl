b4b8bba692cc79ff89bac8104fc1b16a789d54cd
Appcelerator <Raju@Aminul.local>
2017-04-27 20:39:59 +0600

    first commit

---
+++ conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated ArrowDB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "F3FnvXdNjX5c4V0JqprRU1GLPcg1djBc",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "lKhSR6yQAOk7HezDyN"
+    }
+  }
+};
